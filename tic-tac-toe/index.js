c1f3950395edc6f44da9c6ea206df3b92e0ccbb2
HugoFranceschi <franceschi.hugo@yahoo.com>
2025-02-04 15:57:57 +0100

    html-css

---
+++ tic-tac-toe/index.js
@@ -0,0 +1,13 @@
+const CROIT_ID = "CROIT";
+const ROND_ID = "ROND";
+
+const CROIT_IMG = "./asset/croit.svg";
+const ROND_IMG = "./asset/rond.svg";
+
+const casseButton = document.querySelectorAll("casse");
+
+let clickedButton = null;
+let clickedImageSrc = "";
+let clickedName = "";
+
+casseButton.addEventListener("click", (e) => {});
