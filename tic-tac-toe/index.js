3eb733775c6adff424218b42b3a4a6b772eab240
HugoFranceschi <franceschi.hugo@yahoo.com>
2025-02-07 19:08:18 +0100

    js

--- tic-tac-toe/index.js
+++ tic-tac-toe/index.js
@@ -74,31 +74,165 @@ const CROIT_ID = "CROIT";
 const ROND_ID = "ROND";
 const AIR_ID = "AIR"
 
-const CROIT_IMG = "./asset/croit.svg";
-const ROND_IMG = "./asset/rond.svg";
-const AIR_IMG = "./asset/air.webp"
-
+const CROIT_IMG = "asset/croit.svg";
+const ROND_IMG = "asset/rond.svg";
+const AIR_IMG = "asset/air.webp"
 
 const casseButton = document.querySelectorAll(".casse");
+const casseButtton1 = document.querySelectorAll(".casseCroit");
+const casseButtton2 = document.querySelectorAll(".casseRond");
+
+for (let i = 0; i < casseButtton2.length; i++){
+	casseButtton2[i].classList.remove("casseRond")
+}
 
 let clickedButton = null;
 let clickedImageSrc = "";
 let clickedName = "";
+let conteur = 0
 
 for (let i = 0; i < casseButton.length; i++){
+	casseButton[i].querySelector("img")
+	let img = casseButton[i].querySelector("img")
+
 	casseButton[i].addEventListener("click", (e) => {
-		let conteur = 0
-		if (conteur == 0){
-			clickedImageSrc = CROIT_IMG
-			clickedName = CROIT_ID
-			conteur++
-		} else {
-			clickedImageSrc = ROND_IMG
-			clickedName = ROND_ID
-			conteur = conteur - 1
+		console.log(img.src)
+
+	if (img.src.includes(AIR_IMG)){
+			if (conteur == 0){
+				clickedImageSrc = CROIT_IMG
+				clickedName = CROIT_ID
+				conteur = 1
+				console.log(conteur)
+				for (let y = 0; y < casseButtton1.length; y++){
+					casseButtton2[y].classList.add("casseRond")
+					casseButtton1[y].classList.remove("casseCroit")
+				}	
+			} else {
+				clickedImageSrc = ROND_IMG
+				clickedName = ROND_ID
+				conteur = 0
+				console.log(conteur)
+				for (let z = 0; z < casseButtton2.length; z++){
+					casseButtton1[z].classList.add("casseCroit")
+					casseButtton2[z].classList.remove("casseRond")			
+				}		
+			}
+			casseButton[i].querySelector("img").src = clickedImageSrc
+			console.log(clickedImageSrc)
 		}
-		casseButton[i].querySelector("img").src =  clickedImageSrc
-		console.log(clickedImageSrc)
-	});
+	})
 }
 
+const victoir = [
+	[ /*###
+		ooo
+		ooo*/
+		true,
+		true,
+		true,
+		false,
+		false,
+		false,
+		false,
+		false,
+		false
+	]
+	[ /*ooo
+		###
+		ooo*/
+		false,
+		false,
+		false,
+		true,
+		true,
+		true,
+		false,
+		false,
+		false
+	]
+	[ /*ooo
+		ooo
+		###*/
+		false,
+		false,
+		false,
+		false,
+		false,
+		false,
+		true,
+		true,
+		true
+	]
+	[ /*#oo
+		o#o
+		oo#*/
+		true,
+		false,
+		false,
+		false,
+		true,
+		false,
+		false,
+		false,
+		true
+	]
+	[ /*oo#
+		o#o
+		#oo*/
+		false,
+		false,
+		true,
+		false,
+		true,
+		false,
+		true,
+		false,
+		false
+	]
+	[ /*#oo
+		#oo
+		#oo*/
+		true,
+		false,
+		false,
+		true,
+		false,
+		false,
+		true,
+		false,
+		false
+	]
+	[ /*oo#
+		oo#
+		oo#*/
+		false,
+		false,
+		true,
+		false,
+		false,
+		true,
+		false,
+		false,
+		true
+	]
+	[ /*o#o
+		o#o
+		o#o*/
+		false,
+		true,
+		false,
+		false,
+		true,
+		false,
+		false,
+		true,
+		false
+	]
+]
+
+for (let i = 0; i < casseButton.length; i++){
+	if (casseButton[i] == victoir){
+		
+	}
+}
